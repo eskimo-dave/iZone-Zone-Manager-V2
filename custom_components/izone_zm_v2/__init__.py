8a02096be8434cf0926eded853f9f6a29b3e69aa
David Ratten <david.ratten@gmail.com>
2026-09-18 11:14:49 +0000

    type checking added

--- custom_components/izone_zm_v2/__init__.py
+++ custom_components/izone_zm_v2/__init__.py
@@ -1,11 +1,15 @@
 """The iZone Zone Manager V2 integration."""
+
 from __future__ import annotations
 
 import logging
+from typing import TYPE_CHECKING
 
-from homeassistant.config_entries import ConfigEntry
+if TYPE_CHECKING:
+    from homeassistant.config_entries import ConfigEntry
+    from homeassistant.core import HomeAssistant
+
 from homeassistant.const import CONF_HOST, Platform
-from homeassistant.core import HomeAssistant
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 
 from .api import IZoneApiClient
